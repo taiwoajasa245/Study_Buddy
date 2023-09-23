7085339eae685b9070639ee82c1988b858b3ab43
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-09-23 20:58:02 +0100

    code updated

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,6 @@
 const footer = document.getElementById("foot"); 
 const fullScreenLogo = document.getElementById("full-screen"); 
+const reload = document.getElementById('reload'); 
 
 const showHidden = () => { 
     fullScreenLogo.style.display = 'block'; 
@@ -9,3 +10,9 @@ const showHidden = () => {
 function hideEle() { 
     fullScreenLogo.style.display = "none"; 
 }; 
+
+function rotateMe() { 
+    reload.classList.add('animate-spin'); 
+};
+
+// reload.addEventListener('click', rotateMe(), {once: true})

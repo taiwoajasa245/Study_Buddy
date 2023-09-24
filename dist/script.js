59db647528e302eb1ee54a80e8ae4d59a28d15ab
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-09-24 01:00:38 +0100

    code updated

--- dist/script.js
+++ dist/script.js
@@ -12,55 +12,59 @@ function hideEle() { fullScreenLogo.style.display = "none" };
 let min = document.getElementById('min'); 
 let sec = document.getElementById('sec');
 let interval_01; 
+let minl;  
+let secl;
+function countDown() { 
+    reload.classList.remove('animate-spin'); 
 
-function pomodoro() { 
-    function countDown() { 
-        let minl = 25;  
-        let secl = 60;
-        // reduce minute 
-        minl -= 1;
-        min.textContent = minl;
+    // reduce minute 
+    minl = 25;
+    minl -= 1;
+    min.textContent = minl;
+
+    
+  
+
+    // reduce second every 1 second
+    secl = 60; 
+    interval_01 = setInterval(() => { 
+        // decrement second 
+        secl -= 1; 
+        sec.textContent = secl;
+
+        if (secl < 10 ) { sec.textContent = `0${secl}` }; 
         
-        function callerFunc() { 
-            minl = 25;  
-            secl = 60;
+        if (secl < 0) { 
+            secl = 60 - 1;
+            sec.textContent = secl; 
+
+            minl -= 1; 
+            min.textContent = minl; 
+        }; 
+
+        if (minl < 10 ) { min.textContent = `0${minl}` }
+
+    }, 1000);
+
+    if (minl == 0 && secl == 0) { clearInterval(interval_01) }; 
     
-            // min.textContent = minl; 
-            // sec.textContent = secl; 
-        }
-    
-        // reduce second every 1 second
-        interval_01 = setInterval(() => { 
-            // decrement second 
-            secl -= 1; 
-            sec.textContent = secl;
-    
-            if (secl < 10 ) { sec.textContent = `0${secl}` }; 
-            
-            if (secl < 0) { 
-                secl = 60 - 1;
-                sec.textContent = secl; 
-    
-                minl -= 1; 
-                min.textContent = minl; 
-            }; 
-    
-            if (minl < 10 ) { min.textContent = `0${minl}` }
-    
-        }, 1000);
-    
-        if (minl == 0 && secl == 0) { clearInterval(interval_01) }; 
-        
-    }; 
-    
-    timer.addEventListener('click', () => { clearInterval(interval_01) }); 
-    
-    function reloadMe() { 
-    
-        reload.classList.add('animate-spin'); 
-        callerFunc()
-        
-    };
+}; 
+
+timer.addEventListener('click', () => { 
+    clearInterval(interval_01) 
+    reload.classList.remove('animate-spin'); 
+ 
+}); 
+
+function reloadMe() { 
+    reload.classList.add('animate-spin'); 
+    clearInterval(interval_01)
+    minl = 25;  
+    secl = `0`+ 0;
+    min.textContent = minl; 
+    sec.textContent = secl; 
+};
+
+function pauseTimer() { 
     
 }
-document.addEventListener('load', ()=>{pomodoro()}); 

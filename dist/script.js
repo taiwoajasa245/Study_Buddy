86bc867ed9eeb90c42c87ad699383fe419cd44f1
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-09-26 22:49:07 +0100

    code update

--- dist/script.js
+++ dist/script.js
@@ -2,7 +2,7 @@ const footer = document.getElementById("foot");
 const fullScreenLogo = document.getElementById("full-screen"); 
 const reload = document.getElementById('reload'); 
 const timer = document.getElementById('timer'); 
-const startBtn = document.getElementById('start_btn'); 
+
 
 const showHidden = () => { fullScreenLogo.style.display = 'block';}; 
 function hideEle() { fullScreenLogo.style.display = "none" }; 
@@ -16,6 +16,31 @@ let isPaused = false;
 let minl;  
 let secl;
 
+function startInterval() {
+    if (!isPaused) {
+        interval_01 = setInterval(() => { 
+            // decrement second 
+            secl -= 1; 
+            sec.textContent = secl;
+    
+            if (secl < 10 ) { sec.textContent = `0${secl}` }; 
+            
+            if (secl < 0) { 
+                secl = 60 - 1;
+                sec.textContent = secl; 
+    
+                minl -= 1; 
+                min.textContent = minl; 
+            }; 
+    
+            if (minl < 10 ) { min.textContent = `0${minl}` }
+    
+        }, 1000);
+    
+        if (minl == 0 && secl == 0) { clearInterval(interval_01) }; 
+    }
+}
+
 function countDown() { 
     reload.classList.remove('animate-spin'); 
 
@@ -23,33 +48,8 @@ function countDown() {
     minl = 25;
     minl -= 1;
     min.textContent = minl;  
-
     // reduce second every 1 second
     secl = 60; 
-    function startInterval() {
-        if (!isPaused) {
-            interval_01 = setInterval(() => { 
-                // decrement second 
-                secl -= 1; 
-                sec.textContent = secl;
-        
-                if (secl < 10 ) { sec.textContent = `0${secl}` }; 
-                
-                if (secl < 0) { 
-                    secl = 60 - 1;
-                    sec.textContent = secl; 
-        
-                    minl -= 1; 
-                    min.textContent = minl; 
-                }; 
-        
-                if (minl < 10 ) { min.textContent = `0${minl}` }
-        
-            }, 1000);
-        
-            if (minl == 0 && secl == 0) { clearInterval(interval_01) }; 
-        }
-    }
     startInterval();
    
     
@@ -68,6 +68,7 @@ function reloadMe() {
     clearInterval(interval_01)
     minl = 25;  
     secl = `0`+ 0;
+    startBtn.style.display = "block"; 
     min.textContent = minl; 
     sec.textContent = secl; 
 };
@@ -85,12 +86,31 @@ function pauseTimer() {
 // Function to resume the interval
 function resumeTimer() {
     if (isPaused) {
-        console.log(countDown());
-        countDown(); 
         isPaused = false;
+        startInterval(); 
     }
 
 } 
+
+/// seeting up the sart, pause  and resume button 
+const startBtn = document.getElementById("let's-go"); 
+const pauseMeBtn = document.getElementById('pause-me'); 
+const resumeMeBtn = document.getElementById('resume-me'); 
+
+startBtn.addEventListener('click', () =>  {
+    pauseMeBtn.style.display = "block"; 
+    startBtn.style.display = "none"; 
+}); 
+pauseMeBtn.addEventListener('click', () =>  {
+    pauseMeBtn.style.display = "none";
+    resumeMeBtn.style.display = "block"; 
+}); 
+resumeMeBtn.addEventListener('click', () =>  {
+    pauseMeBtn.style.display = "block"; 
+    resumeMeBtn.style.display = "none"; 
+}); 
+
+
 
 /////// settting up the f**k modal
 
